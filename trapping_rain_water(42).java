78ab0ae50b00e0c7b5f231f59ad9680b7f40f282
Shreyas S H <shreyas11.2006@gmail.com>
2026-04-07 20:45:53 +0530

    initial commit

---
+++ trapping_rain_water(42).java
@@ -0,0 +1,32 @@
+public class trapping_rain_water {
+
+    public static int trap(int[] height) {
+        if (height == null || height.length == 0)
+            return 0;
+
+        int l = 0, r = height.length - 1;
+        int leftMax = height[l], rightMax = height[r];
+        int res = 0;
+
+        while (l < r) {
+            if (leftMax < rightMax) {
+                l++;
+                leftMax = Math.max(leftMax, height[l]);
+                res += leftMax - height[l];
+            } else {
+                r--;
+                rightMax = Math.max(rightMax, height[r]);
+                res += rightMax - height[r];
+            }
+        }
+
+        return res;
+    }
+
+    public static void main(String[] args) {
+        int[] height = { 0, 1, 0, 2, 1, 0, 1, 3, 2, 1, 2, 1 };
+
+        int result = trap(height);
+        System.out.println("Trapped water: " + result);
+    }
+}
